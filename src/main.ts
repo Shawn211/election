329164beb16f3b5fc87dc4fee62f7c0ee927e52b
Shawn211 <csy291422638@gmail.com>
2022-10-16 15:10:37 +0800

    Add exception filter handler

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ConfigService } from './services/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   const configService = app.get(ConfigService);
   const port = configService.getPort();
   await app.listen(port);
