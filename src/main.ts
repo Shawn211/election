b8cb72af35c9d0be8a3a7049814f2e8218d59ee9
Shawn211 <csy291422638@gmail.com>
2022-10-17 11:51:18 +0800

    Add swagger API document builder

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import { HttpExceptionFilter } from './filters/http-exception.filter';
@@ -8,6 +9,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const options = new DocumentBuilder()
+    .setTitle('Election')
+    .setDescription('The election API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .addTag('election')
+    .addTag('candidate')
+    .addTag('vote')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.getPort();
